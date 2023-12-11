d333d24f1227a2522816d34addc90f4e004ccace
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-12-11 13:51:40 +0530

    Added-firebase-to-store-profile-pics

--- Ng-Frontend/Task-Supervision-App/src/environments/environment.ts
+++ Ng-Frontend/Task-Supervision-App/src/environments/environment.ts
@@ -1,4 +1,12 @@
 export const environment = {
     production: false,
-    
+    firebaseConfig :{
+        apiKey: "AIzaSyAvchcbMMlUTZo3J3ArPvKALM-iDeYB1zE",
+        authDomain: "voyages-57df7.firebaseapp.com",
+        projectId: "voyages-57df7",
+        storageBucket: "voyages-57df7.appspot.com",
+        messagingSenderId: "369189461372",
+        appId: "1:369189461372:web:32127c50713441dc6a58f9",
+        measurementId: "G-25528K922M"
+      }
   };
